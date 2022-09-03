edcc373acdaed1deda24da56dee65410f45c7468
David Edmondson <threehams@gmail.com>
2022-09-02 20:03:59 -0700

    more of a test

--- cypress/e2e/thing.cy.ts
+++ cypress/e2e/thing.cy.ts
@@ -1,10 +1,15 @@
 import { describe, it, cy } from "local-cypress";
+import { range } from "lodash";
 
 describe("things", () => {
   it("makes a thing", () => {
     cy.visit("/");
     cy.findByText("You have 0 things.");
-    cy.findByText("Make a thing").click();
-    cy.findByText(/You have 1 thing/i);
+    for (const index of range(0, 10)) {
+      cy.findByText("Make a thing").click();
+    }
+    cy.findByText(/You have 10 things/i);
+    cy.findByText(/Make a thing maker/i).click();
+    cy.findByText(/Making 1 thing per second/);
   });
 });
